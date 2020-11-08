225cf815b721cacec87e2e24ca36b3c62763b149
zhongyang9 <2812913296@qq.com>
2020-11-08 14:14:17 +0800

    完成登录界面的布局编写

---
+++ app/src/main/java/com/zhongyang/userlogindemo/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.zhongyang.userlogindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //设置状态栏为透明状态
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+    }
+}
